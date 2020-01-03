530e33cdd4ee582772c3036e56bb10b6bf4a36ff
Gellen <18410471+gellendewanta@users.noreply.github.com>
2020-01-03 11:50:41 +0700

    Fix bug

--- application/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-$route['default_controller'] = 'beranda/index';
-
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
