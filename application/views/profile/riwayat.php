b27700ee3c99a8d7c74e424e308460c9688c770d
Gellen <18410471+gellendewanta@users.noreply.github.com>
2020-01-09 16:46:53 +0700

    Fix Dashboard

--- application/views/profile/riwayat.php
+++ application/views/profile/riwayat.php
@@ -2,42 +2,68 @@
 <div class="container-fluid">
 
 
-    <h1 class="h3 mb-4 text-gray-800">RIWAYAT</h1>
-    Lengkapi data diri anda agar dapat menyewa lebih banyak barang
-    
+    <h1 class="h3 mb-4 text-gray-800">Riwayat Saya</h1>
+
     <div class="row">
-        <div class="col-md-7">
-            
-            <table class="table">
-                <thead class="thead-light">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="col-auto">
+            <div class="card shadow border-left-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Barang yang sudah pernah disewa</h5>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="align-middle">#</th>
+                                <th scope="col" class="align-middle">Nama Barang</th>
+                                <th scope="col" class="align-middle">Jenis</th>
+                                <th scope="col" class="align-middle">Harga</th>
+                                <th scope="col" class="align-middle">Gambar</th>
+                                <th scope="col" class="align-middle">Tanggal Sewa</th>
+                                <th scope="col" class="align-middle">Lama Sewa</th>
+                                <th scope="col" class="align-middle">Terakhir Disewa Oleh</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th class="align-middle" scope="row">1</th>
+                                <td class="align-middle">Kamera</td>
+                                <td class="align-middle">Elektronik</td>
+                                <td class="align-middle">Rp. 150.000,-</td>
+                                <td class="align-middle">
+                                    <img src="<?= base_url('assets/img/product/sewa/camera.jpg') ?>" width="100px" height="100px" class="img-thumbnail">
+                                </td>
+                                <td class="align-middle">
+                                    Jumat, 10 Januari 2020
+                                </td>
+                                <td class="align-middle">
+                                    8 Hari
+                                </td>
+                                <td class="align-middle">
+                                    mahsa
+                                </td>
+                            </tr>
+                            <tr>
+                                <th class="align-middle" scope="row">2</th>
+                                <td class="align-middle">HDD External</td>
+                                <td class="align-middle">Elektronik</td>
+                                <td class="align-middle">Rp. 250.000,-</td>
+                                <td class="align-middle">
+                                    <img src="<?= base_url('assets/img/product/sewa/harddisk.jpg') ?>" width="100px" height="100px" class="img-thumbnail">
+                                </td>
+                                <td class="align-middle">
+                                    Kamis, 09 Desember 2020
+                                </td>
+                                <td class="align-middle">
+                                    14 Hari
+                                </td>
+                                <td class="align-middle">
+                                    ji
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
     </div>
 
